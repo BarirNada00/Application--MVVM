f753995259eb05453e27f0ebc58d61ebda9f5224
BarirNada00 <116973510+BarirNada00@users.noreply.github.com>
2023-02-07 11:46:20 +0100

    Add files via upload

---
+++ viewmodels/LoginViewModel.java
@@ -0,0 +1,59 @@
+package ma.projet.android.architecturemvvm.viewmodels;
+
+public class LoginViewModel extends BaseObservable {
+
+    private User user;
+
+    private String successMessage = "Login successful";
+    private String errorMessage = "Email or Password is not valid";
+    @Bindable
+
+    private String toastMessage = null;
+
+    public LoginViewModel() {
+
+        user = new User("", "");
+    }
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    @Bindable
+    public String getUserEmail() {
+        return user.getEmail();
+    }
+
+    public void setUserEmail(String email) {
+        user.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+
+    @Bindable
+    public String getUserPassword() {
+        return user.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        user.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public void onButtonClicked() {
+        if (isValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+
+    public boolean isValid() {
+        return !TextUtils.isEmpty(getUserEmail()) &&
+                Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches()
+                && getUserPassword().length() > 5;
+    }
+}
